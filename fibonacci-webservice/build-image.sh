9cd0875fdb1243f49ff98d558f364023cbcd0224
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-20 15:01:44 +0000

    Added: pipeline for fibonacci-webservice.

--- fibonacci-webservice/build-image.sh
+++ fibonacci-webservice/build-image.sh
@@ -2,4 +2,8 @@
 
 set -xeuo pipefail
 
-docker build -t fibonacci-webservice:latest .
+# Extract the version from conanfile.py
+VERSION=$(grep -Po '(?<=version = ")[^"]*' conanfile.py)
+
+# Build the Docker image and tag it with both "latest" and the version
+docker build -t fibonacci-webservice:latest -t fibonacci-webservice:"$VERSION" .
