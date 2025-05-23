eff72649c30ca6100d3d3f5ffbb235e9fd0aaba1
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-23 14:53:16 +0000

    Fixed: unknown flag --all-tags. Podman doesn't support it.

--- fibonacci-webservice/build-image.sh
+++ fibonacci-webservice/build-image.sh
@@ -2,8 +2,9 @@
 
 set -xeuo pipefail
 
-# Extract the version from conanfile.py
 VERSION=$(grep -Po '(?<=version = ")[^"]*' conanfile.py)
 
-# Build the Docker image and tag it with both "latest" and the version
 docker build -t registry:5000/fibonacci-webservice:latest -t registry:5000/fibonacci-webservice:"$VERSION" .
+
+docker push registry:5000/fibonacci-webservice:latest
+docker push registry:5000/fibonacci-webservice:"$VERSION"
