9c043154a7438d2922723dda8b7a23798ba2d4c4
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-23 14:49:46 +0000

    Added: pushing image to private registry.

--- fibonacci-webservice/build-image.sh
+++ fibonacci-webservice/build-image.sh
@@ -6,4 +6,4 @@ set -xeuo pipefail
 VERSION=$(grep -Po '(?<=version = ")[^"]*' conanfile.py)
 
 # Build the Docker image and tag it with both "latest" and the version
-docker build -t fibonacci-webservice:latest -t fibonacci-webservice:"$VERSION" .
+docker build -t registry:5000/fibonacci-webservice:latest -t registry:5000/fibonacci-webservice:"$VERSION" .
