76e0df6fa80771de54eead2130ae46d9985874d4
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-25 06:02:04 +0000

    Changed: using latest version of fibonacci library.

--- fibonacci-webservice/conanfile.py
+++ fibonacci-webservice/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class fibonacci_webserviceRecipe(ConanFile):
     name = "fibonacci-webservice"
-    version = "1.0.0"
+    version = "1.0.2"
     package_type = "application"
 
     # Optional metadata
@@ -23,7 +23,7 @@ class fibonacci_webserviceRecipe(ConanFile):
         self.requires("oatpp/1.3.0")
         self.requires("oatpp-swagger/1.3.0")
         self.requires("nlohmann_json/3.12.0")
-        self.requires("fibonacci/1.0.1")
+        self.requires("fibonacci/1.0.2")
 
     def build_requirements(self):
         self.test_requires("gtest/1.14.0")
