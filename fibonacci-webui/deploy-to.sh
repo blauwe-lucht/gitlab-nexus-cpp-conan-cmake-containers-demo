b79a68d9f39a04023e5b72164e30bacb9d3b1ca7
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-05-27 11:24:27 +0000

    Fixed: app version is now available in container without access to git.

--- fibonacci-webui/deploy-to.sh
+++ fibonacci-webui/deploy-to.sh
@@ -3,14 +3,12 @@
 set -euo pipefail
 
 # Cmdline argument is the target environment.
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <environment>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <environment> <version>"
     exit 1
 fi
 TARGET_ENV="$1"
-
-cd "$(dirname "$0")"
-VERSION=$(./get-version.sh)
+VERSION="$2"
 
 # Ansible doesn't like to run in a world-writable directory, so remove those write bits.
 chmod -R o-w ansible
